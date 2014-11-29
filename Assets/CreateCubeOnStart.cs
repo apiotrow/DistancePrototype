c696fa327a71684fefda89b97cad207b8c722fb9
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-11-29 13:23:18 -0800

    ball

---
+++ Assets/CreateCubeOnStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCubeOnStart : Photon.MonoBehaviour {
+	private PhotonView myPhotonView;
+
+	// Use this for initialization
+	void Start () {
+		GameObject block = PhotonNetwork.Instantiate("Cube", new Vector3(0f, 3f, 0f), Quaternion.identity, 0);
+		myPhotonView = block.GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
